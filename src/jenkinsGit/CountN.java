92f6c01e1c1a1884bc1ae99e842ed3ea133a69ae
Dikshyant Rai <drai@Dikshyants-MacBook-Pro.local>
2017-06-12 13:08:10 -0400

    v3

--- src/jenkinsGit/CountN.java
+++ src/jenkinsGit/CountN.java
@@ -5,5 +5,10 @@ public class CountN {
 		for(int i = 0; i < 10; i++){
 			System.out.println(i);
 		}
+		for(int j = 1; j < 100; j++){
+			if(j%2 == 0){
+				System.out.println(j);
+			}
+		}
 	}
 }
